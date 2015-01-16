a07609def39bd22e2755ceb77bff777d481aef80
Milan Milanov <milanov@null.net>
2015-01-16 23:47:01 +0200

    Switch grid implementation from custom to Handsontable

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,6 +3,13 @@ $(function() {
 
     $('.dropdown-submenu > a').submenupicker();
 
-    createSpreadsheet($('#spreadsheet'), 20, 15);
-});
+    var container = document.getElementById('spreadsheet-one');
+    var hot = new Handsontable(container, {
+        startRows: 50,
+        startCols: 26,
+        rowHeaders: true,
+        colHeaders: true,
+        contextMenu: true
+    });
 
+});
