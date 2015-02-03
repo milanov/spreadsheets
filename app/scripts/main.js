790dd6e0dba943724da725511bbe8ac4a3727660
Krasimira <krasimira_bozhanova@abv.bg>
2015-02-03 21:45:56 +0100

    Add tabs for supporting multiple sheets

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,20 +4,46 @@ $(function() {
     /* Activate Bootstrap's submenu plugin */
     $('.dropdown-submenu > a').submenupicker();
 
-    var spreadsheet = $('#spreadsheet-one');
+    var numberOfSpreadsheets = 1;
+
     var spreadsheetToolbar = $('#spreadsheet-one-toolbar');
 
-    /* Manually calculate and set spreadsheet height, needed for the overflow scrolling functionality */
+    /* Instantiate "toolbar" for controlling the Handsontable instance */
+    var htToolbar = new HandsontableToolbar(spreadsheetToolbar[0], 
+            createHandsontableSpreadsheet('spreadsheet-1', '1'));
+
+    var plusTab = $('#js-plus-tab');
+
+    /* TODO: Add the ability to remove a tab */
+    plusTab.on('click', function() {
+        var index = ++numberOfSpreadsheets;
+        var spreadsheetId = "spreadsheet-" + index;
+
+        $('#tab-list li').removeClass('active');
+        $('#tab-list').append('<li class="active"><a href="#' + spreadsheetId + '" data-toggle="tab">Sheet ' + index + '</a></li>');
+
+        /* Instantiate "toolbar" for controlling the Handsontable instance */
+        var htToolbar = new HandsontableToolbar(spreadsheetToolbar[0], 
+            createHandsontableSpreadsheet(spreadsheetId, index));
+    });
+});
+
+function createHandsontableSpreadsheet(id, index, parentSpreadsheet) {
+
+    var newSpreadsheet = $('#spreadsheet-hidden').clone();
+    newSpreadsheet.attr('id', id);
+    newSpreadsheet.removeClass('hidden');
+
     var calculatedSpreadsheetHeight = $(window).height() - $('header').height() - $('footer').height();
-    spreadsheet.height(calculatedSpreadsheetHeight);
+    newSpreadsheet.height(calculatedSpreadsheetHeight);
 
-    var spreadsheetObj = new Spreadsheet(50, 26, {});
-    spreadsheetObj.recomputeAll(null);
+    $('#tab-content div').removeClass('active');
+    $('#tab-content').append(newSpreadsheet);
 
+    var spreadsheetObj = new Spreadsheet(50, 28, parentSpreadsheet);
     var SpreadsheetEditor = getSpreadsheetEditor(spreadsheetObj);
 
-    /* Instantiate and configure Handsontable instance */
-    var ht = new Handsontable(spreadsheet[0], {
+    var ht = new Handsontable(newSpreadsheet[0], {
         autoColumnSize: false,
         startRows: 50,
         startCols: 28,
@@ -30,6 +56,5 @@ $(function() {
     });
     ht.selectCell(0, 0);
 
-    /* Instantiate "toolbar" for controlling the Handsontable instance */
-    var htToolbar = new HandsontableToolbar(spreadsheetToolbar[0], ht);
-});
+    return ht;
+}
